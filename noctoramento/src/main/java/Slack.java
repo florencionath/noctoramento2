effcbbd6b8850a4f94315c22c082c4c243d37973
florencionath <nathalia.pinto@sptech.school>
2024-06-12 01:09:23 -0300

    aplicação sem slack

--- noctoramento/src/main/java/Slack.java
+++ noctoramento/src/main/java/Slack.java
@@ -7,7 +7,7 @@ import java.net.http.HttpResponse;
 public abstract class Slack {
     public static void EnviarAlertaSlack(String mensagemErro){
         // URL do webhook do Slack
-        final String slackWebhookUrl = "https://hooks.slack.com/services/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";
+        final String slackWebhookUrl = "";
 
         HttpClient client = HttpClient.newHttpClient();
 
